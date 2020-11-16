880324c056d2b6cd25399de586e98a73443f3f92
alex <alex7chiaster@gmail.com>
2020-11-17 00:13:19 +0800

    added an important note regarding renaming the controllers folder at index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './controllers/App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./controllers/App";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// TAKE NOTE: Changing 'controllers' folder name to anything like 'containers' screws up when pushing codebase to Heroku server. I don't why yet...
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
