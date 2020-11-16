9e79194673bbe1049440b9c63f11b9ff8acff081
alex <alex7chiaster@gmail.com>
2020-11-16 23:32:36 +0800

    trying to salvage changes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Face Detection App (Front-end)
+
 `Live:` https://devchia254-facedetection.herokuapp.com/
 
 ![App Snapshot](./README_resources/gif-loginpage.gif)
@@ -6,111 +7,115 @@
 A web app that can detect multiple faces submitted from an image URL. This is the source code of the **Front-end**. To see the Back-end, [**click here**](https://github.com/devchia254/facedetection-api).
 
 ## Info
+
 - The Front-end is created using the `create-react-app `boilerplate from [Facebook](https://github.com/facebook/create-react-app).
 - This web app uses the face recognition feature from the [Clarifai API](https://www.clarifai.com/models/face-detection-image-recognition-model-a403429f2ddf4b49b307e318f00e528b-detection) but is stored in the back-end.
 - The web app is not mobile responsive (yet!).
 
-
 ## Purpose
+
 - Learn how to create Full Stack web app using React, Node.js and PostgreSQL.
 
 ## Features & Code Snippets
+
 Below are some of the features and code extracts of this project.
 
 ### Multiple Face Recognition Feature
+
 ---
+
 ![AJAX Fetching](./README_resources/gif-faceDetect.gif)
 
-When faces are detected from the image submitted, they are recognized and calculated based on coordinates called "bounding boxes". These bounding boxes are then stored in an array. 
+When faces are detected from the image submitted, they are recognized and calculated based on coordinates called "bounding boxes". These bounding boxes are then stored in an array.
 
 ##### App.js:
+
 ```javascript
 calculateFaceLocation = (data) => {
+  const arrOfBoundingBoxes = []; // Stores an array of bounding box objects
+  const regions = data.outputs[0].data.regions; // Easy access to the bounding boxes once the faces has been detected from the image URL
 
-	const arrOfBoundingBoxes = []; // Stores an array of bounding box objects 
-	const regions = data.outputs[0].data.regions; // Easy access to the bounding boxes once the faces has been detected from the image URL
-	
-	//Gets dimensions of the image used for detection
-	const image = document.getElementById('inputimage');
-	const width = Number(image.width);
-	const height = Number(image.height);
-	
-	// Loops through a list of bounding boxes in the "region_info" as objects
-	regions.forEach((region, i) => {
-		const boundingBox = region.region_info.bounding_box;// Retrieve the coordinates of faces detected in the form of bounding boxes
+  //Gets dimensions of the image used for detection
+  const image = document.getElementById("inputimage");
+  const width = Number(image.width);
+  const height = Number(image.height);
 
-		// Calculates the coordinates of the bounding boxes and stores them into an object, then pushes each object into an array
-		arrOfBoundingBoxes.push(
-			{
-				leftCol: boundingBox.left_col * width,
-				topRow: boundingBox.top_row * height,
-				rightCol: width - (boundingBox.right_col * width),
-				bottomRow: height - (boundingBox.bottom_row * height)
-			}
-		)
-	});
-	return arrOfBoundingBoxes; // returns array of bounding boxes for the function
-}
+  // Loops through a list of bounding boxes in the "region_info" as objects
+  regions.forEach((region, i) => {
+    const boundingBox = region.region_info.bounding_box; // Retrieve the coordinates of faces detected in the form of bounding boxes
+
+    // Calculates the coordinates of the bounding boxes and stores them into an object, then pushes each object into an array
+    arrOfBoundingBoxes.push({
+      leftCol: boundingBox.left_col * width,
+      topRow: boundingBox.top_row * height,
+      rightCol: width - boundingBox.right_col * width,
+      bottomRow: height - boundingBox.bottom_row * height,
+    });
+  });
+  return arrOfBoundingBoxes; // returns array of bounding boxes for the function
+};
 ```
+
 ##### FaceRecognition.js:
+
 The FaceRecognition component uses the box `state` as a `props`. The box `state` is an empty array that contains the bounding boxes calculated from the `calculateFaceLocation` function.
 
 ```javascript
 // Creates a new array of bounding box divs to enable multiple face detection
 const listOfFaces = box.map((face, i) => {
-	const { topRow, rightCol, bottomRow, leftCol } = face;
-	
-	return (
-		<div className='bounding-box'
-			key={i}
-			style={
-				{
-				top: topRow,
-				right: rightCol,
-				bottom: bottomRow,
-				left: leftCol
-				}
-			}
-		>
-		</div>
-	)
+  const { topRow, rightCol, bottomRow, leftCol } = face;
+
+  return (
+    <div
+      className="bounding-box"
+      key={i}
+      style={{
+        top: topRow,
+        right: rightCol,
+        bottom: bottomRow,
+        left: leftCol,
+      }}
+    ></div>
+  );
 });
 ```
+
 ### Connect to the Clarifai API from the Back-end
+
 When the image URL gets submitted, the `onButtonSubmit` function accesses the server through the path '/imageUrl' to connect to the Clarifai API. The returning value of this function triggers the `calculateFaceLocation` to detect the bounding boxes then stores them in the box `state` through the `displayFaceBox` function.
 
 ```javascript
 onButtonSubmit = () => {
-  this.setState({imageUrl: this.state.input}); //Stores the input in the imageUrl state
-// Create a post request for calling on the Clarifai API from the Back-end when the imageURL has been submitted
-  fetch('https://fast-sands-24633.herokuapp.com/imageurl', {
-    method: 'post',
-    headers: {'Content-Type': 'application/json'},
+  this.setState({ imageUrl: this.state.input }); //Stores the input in the imageUrl state
+  // Create a post request for calling on the Clarifai API from the Back-end when the imageURL has been submitted
+  fetch("https://devchia254-facedetection-api.herokuapp.com/imageurl", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      input: this.state.input
-    })
+      input: this.state.input,
+    }),
   })
-    .then(response => response.json())
-    .then(response => {
-// If there is a response, it retrieves the ID of the user who submitted an image then increments the total entry of image submitted by that user.
+    .then((response) => response.json())
+    .then((response) => {
+      // If there is a response, it retrieves the ID of the user who submitted an image then increments the total entry of image submitted by that user.
       if (response) {
-        fetch('https://fast-sands-24633.herokuapp.com/image', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
+        fetch("https://devchia254-facedetection-api.herokuapp.com/image", {
+          method: "put",
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            id: this.state.user.id
-          })
+            id: this.state.user.id,
+          }),
         })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, { entries: count }))
+          .then((response) => response.json())
+          .then((count) => {
+            this.setState(Object.assign(this.state.user, { entries: count }));
           })
-          .catch(console.log)
+          .catch(console.log);
       }
-      this.displayFaceBox(this.calculateFaceLocation(response))// Finally stores the bounding boxes objects into box state, once all above is parsed
+      this.displayFaceBox(this.calculateFaceLocation(response)); // Finally stores the bounding boxes objects into box state, once all above is parsed
     })
-    .catch(err => console.log(err))
- }
+    .catch((err) => console.log(err));
+};
 ```
 
 ## NPM Dev Packages:
--- src/controllers/App.js
+++ src/controllers/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import Navigation from '../components/Navigation/Navigation';
-import Signin from '../components/Signin/Signin';
-import Register from '../components/Register/Register';
-import Logo from '../components/Logo/Logo';
-import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
-import Rank from '../components/Rank/Rank';
-import Navbar from '../components/Navbar/Navbar';
-import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
-import './App.css';
-import Particles from 'react-particles-js';
+import React, { Component } from "react";
+import Navigation from "../components/Navigation/Navigation";
+import Signin from "../components/Signin/Signin";
+import Register from "../components/Register/Register";
+import Logo from "../components/Logo/Logo";
+import ImageLinkForm from "../components/ImageLinkForm/ImageLinkForm";
+import Rank from "../components/Rank/Rank";
+import Navbar from "../components/Navbar/Navbar";
+import FaceRecognition from "../components/FaceRecognition/FaceRecognition";
+import "./App.css";
+import Particles from "react-particles-js";
 
 const particleOptions = {
   particles: {
@@ -16,46 +16,46 @@ const particleOptions = {
       value: 80,
       density: {
         enable: true,
-        value_area: 800
-      }
+        value_area: 800,
+      },
     },
-  }
-}
+  },
+};
 // Change to 'route: home' to bypass the signin component'
 const initialState = {
-  input: '',
-  imageUrl: '',
+  input: "",
+  imageUrl: "",
   box: [], //{} initially. Changing into array[] can store more than one value.
-  route: 'signin',
+  route: "signin",
   loading: false,
   isSignedIn: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
-  }
-}
+    joined: "",
+  },
+};
 
 class App extends Component {
-
   // Assigning to initialState clears the state once signed out. This prevents one user's image submitted in the app, stays in the app. That same image can be seen when another user logs in and trys the app
   constructor() {
     super();
     this.state = initialState;
   }
 
-
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries:data.entries,
-      joined: data.joined
-    }})
-  }
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
     // console.log("data: ", data)
@@ -64,7 +64,7 @@ class App extends Component {
     // console.log("array of bounding boxes:",arrOfBoundingBoxes);
 
     //Gets dimensions of the image used for detection
-    const image = document.getElementById('inputimage');
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
     // console.log("width:", width, "hegiht:", height);
@@ -74,64 +74,64 @@ class App extends Component {
       arrOfBoundingBoxes.push({
         leftCol: boundingBox.left_col * width,
         topRow: boundingBox.top_row * height,
-        rightCol: width - (boundingBox.right_col * width),
-        bottomRow: height - (boundingBox.bottom_row * height)
-      })
+        rightCol: width - boundingBox.right_col * width,
+        bottomRow: height - boundingBox.bottom_row * height,
+      });
     });
     return arrOfBoundingBoxes;
     //Solution could be return a foreach loop for the dimensions of each box to enable multiple face detections.
     // Also change the return statement into an array of objects coz currently, it is only returning one object.
-  }
+  };
 
   displayFaceBox = (box) => {
     // console.log("box:", box);
-    this.setState({box: box});
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
+    this.setState({ input: event.target.value });
+  };
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
+    this.setState({ imageUrl: this.state.input });
 
-    fetch('https://fast-sands-24633.herokuapp.com/imageurl', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
+    fetch("https://devchia254-facedetection-api.herokuapp.com/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        input: this.state.input
-      })
+        input: this.state.input,
+      }),
     })
-      .then(response => response.json())
-      .then(response => {
+      .then((response) => response.json())
+      .then((response) => {
         if (response) {
-          fetch('https://fast-sands-24633.herokuapp.com/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
+          fetch("https://devchia254-facedetection-api.herokuapp.com/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: this.state.user.id
-            })
+              id: this.state.user.id,
+            }),
           })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-            .catch(console.log)
+            .catch(console.log);
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   onRouteChange = (route) => {
     // console.log("route: ", route);
-    if (route === 'signout') {
-      this.setState(initialState);  
-    } else if (route === 'home') {
-      this.setState({isSignedIn: true});
+    if (route === "signout") {
+      this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route});
-  }
+    this.setState({ route: route });
+  };
 
   render() {
     const { isSignedIn, imageUrl, route, box, loading } = this.state;
@@ -142,48 +142,56 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Particles 
-          className='particles'
-          params={particleOptions}
-        />
+        <Particles className="particles" params={particleOptions} />
         <div className="navbar">
           <Navbar>
             <Logo />
-            <Rank isSignedIn={isSignedIn} name={this.state.user.name} entries={this.state.user.entries} box={box} />
-            <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+            <Rank
+              isSignedIn={isSignedIn}
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+              box={box}
+            />
+            <Navigation
+              isSignedIn={isSignedIn}
+              onRouteChange={this.onRouteChange}
+            />
           </Navbar>
         </div>
 
-        { route === 'home'
-          ?   <div className='content-page'>
-                <div className='wrapper'>
-                  <div className='wrapper-items link-form'>
-                    <ImageLinkForm 
-                      onInputChange={this.onInputChange} 
-                      onButtonSubmit={this.onButtonSubmit}
-                    />
-                  </div>
-                  <div className='wrapper-items image-section'>
-                          <FaceRecognition 
-                            box={box} 
-                            imageUrl={imageUrl}
-                            loading={loading} 
-                          />
-                  </div>
-                </div>
+        {route === "home" ? (
+          <div className="content-page">
+            <div className="wrapper">
+              <div className="wrapper-items link-form">
+                <ImageLinkForm
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+                />
               </div>
-
-          : ( route === 'signin'
-              ? <div className='content-page'>
-                  <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-                </div>
-                  
-              : <div className='content-page'>
-                  <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-                </div>
-            )
-        }
-
+              <div className="wrapper-items image-section">
+                <FaceRecognition
+                  box={box}
+                  imageUrl={imageUrl}
+                  loading={loading}
+                />
+              </div>
+            </div>
+          </div>
+        ) : route === "signin" ? (
+          <div className="content-page">
+            <Signin
+              loadUser={this.loadUser}
+              onRouteChange={this.onRouteChange}
+            />
+          </div>
+        ) : (
+          <div className="content-page">
+            <Register
+              loadUser={this.loadUser}
+              onRouteChange={this.onRouteChange}
+            />
+          </div>
+        )}
       </div>
     );
   }
